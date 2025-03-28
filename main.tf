cb7d66262db88d5a153b079228d09783915aae87
palpati1000 <pravinalpati1000@Gmail.com>
2025-03-28 21:50:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -70,8 +70,43 @@ resource "aws_security_group" "t_sg" {
     }
 
     ingress {
-      from_port  = 80
-      to_port    = 80
+      from_port  = 2377
+      to_port    = 2377
+      protocol   = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port  = 7946
+      to_port    = 7946
+      protocol   = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port  = 4789
+      to_port    = 4789
+      protocol   = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port  = 9200
+      to_port    = 9200
+      protocol   = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port  = 8288
+      to_port    = 8288
+      protocol   = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port  = 8080
+      to_port    = 8090
       protocol   = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
